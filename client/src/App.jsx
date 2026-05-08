389aa8c3c1677013c247f71157e5c8c7d7d61eae
Daniel <falcdanicodes@gmail.com>
2026-05-08 23:05:52 +0300

    add prisma generate to postinstall and updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
+import Welcome from './pages/Welcome'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
@@ -11,6 +12,7 @@ import NotFound from './pages/NotFound'
 const App = () => {
   return (
     <Routes>
+      <Route path="/home" element={<Welcome/>} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
---
+++ client/src/pages/Welcome.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react'
+
+const Welcome = () => {
+    return <div className='bg-gray-900 min-h-screen text-white text-lg text-center'>
+        Welcome page
+    </div>
+}
+
+export default Welcome
