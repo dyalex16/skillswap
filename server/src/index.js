0c28976c4afd5c8bdd1d4f955d065131cfbae4cc
Daniel <falcdanicodes@gmail.com>
2026-07-14 12:03:37 +0300

    fix trust proxy and database SSL

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,7 @@ import { initSocket } from './socket/chatSocket.js';
 import meetingRoutes from './routes/meetingRoutes.js'
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 
 // Middleware
--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -5,6 +5,7 @@ import pg from 'pg'
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
 
 const adapter = new PrismaPg(pool)
